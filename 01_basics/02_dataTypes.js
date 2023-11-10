3f2b9a43e85d88ee9eee976d5b099756f4762353
ArpitBalodi <80304757+ArpitBalodi@users.noreply.github.com>
2023-11-10 16:46:39 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //Treat all JS code as newer version
+
+// alert(3+3);     //We are using nodejs, not browser
+
+console.log(3+3); 
+
+console.log("Arpit");
+
+let name = "Arpit";
+let age = 21;
+let isLoggedIn = false;
+
+// Number -> 2 to power 53
+// bigint 
+// string -> ""
+// Boolean -> true/false
+// NULL -> standalone value
+// undefined 
+// Symbol ->unique
+
+// Object
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // Undefined
+
