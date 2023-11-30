7c019a573deb83fa8b7fdbd8a501a43384c3e7fc
Arpit Balodi <80304757+ArpitBalodi@users.noreply.github.com>
2023-11-30 17:31:56 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,54 @@
+// const tinderUser = new Object()      -> Singleton Object
+
+const tinderUser = {}      //   -> Non - Singleton Object
+
+tinderUser.id = "123hij";
+tinderUser.name = "Harry";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "123@gmail.com",
+    fullname: {
+        userfullName: {
+            firstName: "Arpit",
+            lastName: "Balodi"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullName.firstName);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1,obj2)
+
+const obj3 = {...obj1,...obj2};
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+]
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
