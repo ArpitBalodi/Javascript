afa00d2fa8cc1262bee653196bfe381ec076684f
Arpit Balodi <80304757+ArpitBalodi@users.noreply.github.com>
2023-12-01 15:11:45 +0000

    Parameters in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,5 +33,34 @@ function loginUserMessage(username = "Harry"){
 }
 
 // console.log(loginUserMessage("Arpit"))
-console.log(loginUserMessage("Arpit"))
+// console.log(loginUserMessage("Arpit"))
 
+function calculateCartPrice(val1,val2,...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(200,400,600,2000));
+
+// const user = {
+//   username: "Arpit",
+//   prices: 199
+// }
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+  username: "Harry",
+  price: 399
+});
+
+const myNewArray = [200, 400, 100, 500];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 500]));
