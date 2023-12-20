052059ec98dfed58a11387a4abfa0d06124b2039
Arpit Balodi <80304757+ArpitBalodi@users.noreply.github.com>
2023-12-20 09:55:53 +0000

    control flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,40 @@
+//if
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if(temperature === 41){
+//     console.log("Temperature is less than 50");
+// }
+// else{
+// console.log("Temperature is greater than 50");
+// }
+// console.log("Execute");
+
+const score = 200;
+
+// if(score > 100){
+//     let power = "fly";
+//     console.log(`User Power: ${power}`);
+// }
+
+// console.log(`User Power: ${power}`);
+
+const balance = 1000;
+// if(balance < 500){
+//      console.log("Less than 500");
+// }
+// else if(balance<800){
+//      console.log("less than 800");
+// }
+// else{
+//     console.log("Less than 1200");
+// }
+
+const userLoggesIn = true;
+const debitcard = true;
+
+if(userLoggesIn && debitcard){
+    console.log("Allow to buy course");
+}
+
+// <, >, <=, >=, ==, !=, ===(also check type), !==
