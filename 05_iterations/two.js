1e50935b97b2302211e5fdcd472f07db94b2f6c7
Arpit Balodi <80304757+ArpitBalodi@users.noreply.github.com>
2023-12-22 08:36:54 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while(index <= 10){
+    //  console.log(`Value of Index is : ${index}`);
+     index = index + 3;
+}
+
+let myArray = ["Flash", "Batman", "Superman", "Shaktiman"]
+let arr = 0;
+while(arr <= myArray.length){
+    // console.log(`Value is : ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 11;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}
+while(score <= 10);
